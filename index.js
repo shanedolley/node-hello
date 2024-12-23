b88c9af41e89f38c35cc37e48de31285b7c97b58
Shane <shanedolley@me.com>
2024-12-23 23:39:26 +1000

    updated test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'This is a change\nHello Node!\n'
+  const msg = 'This is another change\nHello Node!\n'
   res.end(msg);
 });
 
